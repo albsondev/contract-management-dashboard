7d7055c938fc5cee6245f1aff0a03b9066de5aff
André Albson <106167607+albsondev@users.noreply.github.com>
2025-02-06 22:40:38 -0300

    fix align links - responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,15 +7,18 @@ const Header = () => {
       <h1 className="text-xl font-semibold text-gray-800 hidden md:block">Dashboard de Contratos</h1>
       <div className="flex items-center">
         {/* Links de Navegação Centralizados Apenas em Telas Menores */}
-        <div className="md:hidden flex flex-row gap-4 justify-center items-center space-y-2"> {/* Alteração para centralizar */}
+        <div className="md:hidden flex flex-row gap-4 justify-center items-center">
           <Link to="/" className="flex items-center text-gray-600 hover:text-gray-800">
-            <FiHome className="mr-1" /> Dashboard
+            <FiHome className="mr-2 h-5 w-5" /> {/* Ajuste aqui: h-5 w-5 (tamanho do ícone) */}
+            Dashboard
           </Link>
           <Link to="/contracts" className="flex items-center text-gray-600 hover:text-gray-800">
-            <FiFileText className="mr-1" /> Contratos
+            <FiFileText className="mr-2 h-5 w-5" /> {/* Ajuste aqui: h-5 w-5 (tamanho do ícone) */}
+            Contratos
           </Link>
           <Link to="/reports" className="flex items-center text-gray-600 hover:text-gray-800">
-            <FiBarChart2 className="mr-1" /> Relatórios
+            <FiBarChart2 className="mr-2 h-5 w-5" /> {/* Ajuste aqui: h-5 w-5 (tamanho do ícone) */}
+            Relatórios
           </Link>
         </div>
         <span className="text-gray-600 ml-4 hidden md:block">Bem-vindo, Usuário</span>
